f1b8936755680ec67ea896915a7c797352864018
Enemy <kenny.q2@gmail.com>
2024-05-31 15:17:17 -0400

    teslas won't attack turrets

--- horde/g_horde.cpp
+++ horde/g_horde.cpp
@@ -334,7 +334,7 @@ constexpr struct weighted_item_t {
     { "item_sphere_defender", -1, -1, 0.06f, adjust_weight_powerup },
     { "item_sphere_hunter", 9, -1, 0.06f, adjust_weight_powerup },
     { "item_invisibility", 4, -1, 0.08f, adjust_weight_powerup },
-    { "item_doppleganger", 3, -1, 0.05f, adjust_weight_powerup },
+    { "item_doppleganger", 3, -1, 0.068f, adjust_weight_powerup },
 
     { "weapon_chainfist", -1, 3, 0.12f, adjust_weight_weapon },
     { "weapon_shotgun", -1, -1, 0.27f, adjust_weight_weapon },
--- rogue/g_rogue_newweap.cpp
+++ rogue/g_rogue_newweap.cpp
@@ -927,6 +927,10 @@ static BoxEdictsResult_t tesla_think_active_BoxFilter(edict_t* check, void* data
 		g_horde->integer && check->classname && (check->flags & FL_TRAP))
 		return BoxEdictsResult_t::Skip;
 
+	// Don't hit monster_turret
+	if (check->classname && strcmp(check->classname, "monster_turret") == 0)
+		return BoxEdictsResult_t::Skip;
+
 	return BoxEdictsResult_t::Keep;
 }
 
@@ -970,6 +974,10 @@ THINK(tesla_think_active) (edict_t* self) -> void
 				continue;
 		}
 		if (!(hit->svflags & SVF_MONSTER) && !(hit->flags & FL_DAMAGEABLE) && !hit->client)
+			continue;
+
+		// Don't hit monster_turret
+		if (hit->classname && strcmp(hit->classname, "monster_turret") == 0)
 			continue;
 
 		tr = gi.traceline(start, hit->s.origin, self, MASK_PROJECTILE);
