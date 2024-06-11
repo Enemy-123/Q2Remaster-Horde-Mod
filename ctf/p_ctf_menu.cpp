8aa610357ac4d49805c36e4bac5b194667985307
Enemy-123 <kenny.q2@gmail.com>
2024-06-11 04:12:42 -0400

    crashes select this time, trying a fix

--- ctf/p_ctf_menu.cpp
+++ ctf/p_ctf_menu.cpp
@@ -24,24 +24,30 @@ pmenuhnd_t* PMenu_Open(edict_t* ent, const pmenu_t* entries, int cur, int num, v
 	hnd = (pmenuhnd_t*)gi.TagMalloc(sizeof(*hnd), TAG_LEVEL);
 	if (!hnd)
 	{
-		gi.Com_Print("Failed to allocate memory for hnd\n");
+		gi.Com_Print("error: failed to allocate memory for menu handle\n");
 		return nullptr;
 	}
-
 	hnd->UpdateFunc = UpdateFunc;
+
 	hnd->arg = arg;
 	hnd->entries = (pmenu_t*)gi.TagMalloc(sizeof(pmenu_t) * num, TAG_LEVEL);
 	if (!hnd->entries)
 	{
-		gi.Com_Print("Failed to allocate memory for hnd->entries\n");
+		gi.Com_Print("error: failed to allocate memory for menu entries\n");
 		gi.TagFree(hnd);
 		return nullptr;
 	}
-	memcpy(hnd->entries, entries, sizeof(pmenu_t) * num);
 
-	// Duplicate the strings since they may be from static memory
+	memcpy(hnd->entries, entries, sizeof(pmenu_t) * num);
+	// duplicate the strings since they may be from static memory
 	for (i = 0; i < num; i++)
+	{
 		Q_strlcpy(hnd->entries[i].text, entries[i].text, sizeof(entries[i].text));
+		if (hnd->entries[i].text[0] == '\0')
+		{
+			gi.Com_PrintFmt("warning: empty string detected in menu entry %d\n", i);
+		}
+	}
 
 	hnd->num = num;
 
@@ -108,7 +114,7 @@ void PMenu_Close(edict_t* ent)
 }
 
 // only use on pmenu's that have been called with PMenu_Open
-void PMenu_UpdateEntry(pmenu_t *entry, const char *text, int align, SelectFunc_t SelectFunc)
+void PMenu_UpdateEntry(pmenu_t* entry, const char* text, int align, SelectFunc_t SelectFunc)
 {
 	Q_strlcpy(entry->text, text, sizeof(entry->text));
 	entry->align = align;
@@ -117,13 +123,13 @@ void PMenu_UpdateEntry(pmenu_t *entry, const char *text, int align, SelectFunc_t
 
 #include "../g_statusbar.h"
 
-void PMenu_Do_Update(edict_t *ent)
+void PMenu_Do_Update(edict_t* ent)
 {
 	int			i;
-	pmenu_t	*p;
+	pmenu_t* p;
 	int			x;
-	pmenuhnd_t *hnd;
-	const char *t;
+	pmenuhnd_t* hnd;
+	const char* t;
 	bool		alt = false;
 
 	if (!ent->client->menu)
@@ -156,7 +162,7 @@ void PMenu_Do_Update(edict_t *ent)
 
 		sb.yv(32 + i * 8);
 
-		const char *loc_func = "loc_string";
+		const char* loc_func = "loc_string";
 
 		if (p->align == PMENU_ALIGN_CENTER)
 		{
@@ -193,7 +199,7 @@ void PMenu_Do_Update(edict_t *ent)
 	gi.WriteString(sb.sb.str().c_str());
 }
 
-void PMenu_Update(edict_t *ent)
+void PMenu_Update(edict_t* ent)
 {
 	if (!ent->client->menu)
 	{
@@ -213,11 +219,11 @@ void PMenu_Update(edict_t *ent)
 	ent->client->menudirty = true;
 }
 
-void PMenu_Next(edict_t *ent)
+void PMenu_Next(edict_t* ent)
 {
-	pmenuhnd_t *hnd;
+	pmenuhnd_t* hnd;
 	int			i;
-	pmenu_t	*p;
+	pmenu_t* p;
 
 	if (!ent->client->menu)
 	{
@@ -250,11 +256,11 @@ void PMenu_Next(edict_t *ent)
 	PMenu_Update(ent);
 }
 
-void PMenu_Prev(edict_t *ent)
+void PMenu_Prev(edict_t* ent)
 {
-	pmenuhnd_t *hnd;
+	pmenuhnd_t* hnd;
 	int			i;
-	pmenu_t	*p;
+	pmenu_t* p;
 
 	if (!ent->client->menu)
 	{
@@ -290,24 +296,40 @@ void PMenu_Prev(edict_t *ent)
 	PMenu_Update(ent);
 }
 
-void PMenu_Select(edict_t *ent)
+void PMenu_Select(edict_t* ent)
 {
-	pmenuhnd_t *hnd;
-	pmenu_t	*p;
+	pmenuhnd_t* hnd;
+	pmenu_t* p;
 
 	if (!ent->client->menu)
 	{
-		gi.Com_Print("warning:  ent has no menu\n");
+		gi.Com_Print("warning: ent has no menu\n");
 		return;
 	}
 
 	hnd = ent->client->menu;
 
 	if (hnd->cur < 0)
+	{
+		gi.Com_Print("warning: no selectable entries\n");
 		return; // no selectable entries
+	}
 
 	p = hnd->entries + hnd->cur;
 
+	if (!p)
+	{
+		gi.Com_Print("error: null pointer for menu entry\n");
+		return;
+	}
+
 	if (p->SelectFunc)
+	{
 		p->SelectFunc(ent, hnd);
+	}
+	else
+	{
+		gi.Com_Print("error: no SelectFunc found for the current menu entry\n");
+	}
 }
+
