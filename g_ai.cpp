89d6155981d93a067bbc38243e639d7e297ddd1c
Enemy-123 <kenny.q2@gmail.com>
2024-06-13 17:31:11 -0400

    possibly turrets wont go crazy now

--- g_ai.cpp
+++ g_ai.cpp
@@ -786,7 +786,7 @@ bool FindTarget(edict_t* self)
 
     if (!client)
     {
-        if (g_horde->integer) { // this makes turrets go crazy maybe
+        if (g_horde->integer && strcmp(self->classname, "monster_turret")) { // this avoid turrets go crazy maybe
         // Busca enemigos en el equipo contrario
         edict_t* ent = nullptr;
         float range = 1000.0f; // Rango de búsqueda
