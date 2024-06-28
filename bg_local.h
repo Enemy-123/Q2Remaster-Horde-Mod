84d9a14ecb8d2fc5375d80c6a0fda34b3538c875
Enemy-123 <kenny.q2@gmail.com>
2024-06-28 02:54:50 -0400

    lower configstring is the solution?

--- bg_local.h
+++ bg_local.h
@@ -53,7 +53,7 @@ enum coop_respawn_t
 };
 
 constexpr int MAX_PLAYER_CONFIGSTRINGS = 16;
-constexpr int MAX_MONSTER_CONFIGSTRINGS = 84; // 35 monstruos + 50 trampas = 85 en total
+constexpr int MAX_MONSTER_CONFIGSTRINGS = 60;
 constexpr int MAX_CTF_ID_VIEW_CONFIGSTRINGS = 100;
 
 enum {
