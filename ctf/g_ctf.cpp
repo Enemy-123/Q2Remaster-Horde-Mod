6c2b52d22eb78769aebdf6fdb544615355a03fb8
Enemy-123 <kenny.q2@gmail.com>
2024-07-15 02:33:47 -0400

    msg on scoreboard typo

--- ctf/g_ctf.cpp
+++ ctf/g_ctf.cpp
@@ -2088,7 +2088,7 @@ void CTFScoreboardMessage(edict_t* ent, edict_t* killer) {
 		fmt::format_to(std::back_inserter(string), FMT_STRING("ifgef {} yb -48 xv 0 loc_cstring2 0 \"\n\n\nMAKE THEM PAY !!!\" endif "), (level.intermission_server_frame + (5_sec).frames()));
 	}
 	else
-		fmt::format_to(std::back_inserter(string), FMT_STRING("xv 0 yb -48 cstring2 \"{}\" "), "Use Compass/inventory <KEY> to toggle Horde Menu.");
+		fmt::format_to(std::back_inserter(string), FMT_STRING("xv 0 yb -48 cstring2 \"{}\" "), "Use Compass or Inventory <KEY> to toggle Horde Menu.");
 	gi.WriteByte(svc_layout);
 	gi.WriteString(string.c_str());
 }
