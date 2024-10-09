ba92609969194f8f16df583176d81d5b961c5f8e
Enemy-123 <kenny.q2@gmail.com>
2024-10-08 21:35:03 -0300

    restoring ammoregen, part1

--- p_view.cpp
+++ p_view.cpp
@@ -988,6 +988,102 @@ static void G_SetClientEffects(edict_t* ent)
 	}
 #endif
 }
+void HORDE_ApplyAmmoRegen(edict_t* ent) {
+	gclient_t* client;
+
+	if (!g_ammoregen->integer)
+		return;
+
+	client = ent->client;
+	if (!client)
+		return;
+
+	if (!client->ammoregentime) {
+		client->ammoregentime = level.time;
+		return;
+	}
+
+	if (client->ammoregentime < level.time) {
+		client->ammoregentime = level.time + 9000_ms;
+
+		if (client->pers.inventory[IT_WEAPON_SHOTGUN] || client->pers.inventory[IT_WEAPON_SSHOTGUN]) {
+			client->pers.inventory[IT_AMMO_SHELLS] += 8;
+			if (client->pers.inventory[IT_AMMO_SHELLS] > client->pers.max_ammo[AMMO_SHELLS])
+				client->pers.inventory[IT_AMMO_SHELLS] = client->pers.max_ammo[AMMO_SHELLS];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_MACHINEGUN] || client->pers.inventory[IT_WEAPON_CHAINGUN]) {
+			if (g_tracedbullets->integer) {
+				client->pers.inventory[IT_AMMO_BULLETS] += 45;
+			}
+			else {
+				client->pers.inventory[IT_AMMO_BULLETS] += 30;
+			}
+			if (client->pers.inventory[IT_AMMO_BULLETS] > client->pers.max_ammo[AMMO_BULLETS])
+				client->pers.inventory[IT_AMMO_BULLETS] = client->pers.max_ammo[AMMO_BULLETS];
+		}
+
+		client->pers.inventory[IT_AMMO_GRENADES] += 6;
+		if (client->pers.inventory[IT_AMMO_GRENADES] > client->pers.max_ammo[AMMO_GRENADES])
+			client->pers.inventory[IT_AMMO_GRENADES] = client->pers.max_ammo[AMMO_GRENADES];
+
+		if (client->pers.inventory[IT_WEAPON_RLAUNCHER]) {
+			client->pers.inventory[IT_AMMO_ROCKETS] += 6;
+			if (client->pers.inventory[IT_AMMO_ROCKETS] > client->pers.max_ammo[AMMO_ROCKETS])
+				client->pers.inventory[IT_AMMO_ROCKETS] = client->pers.max_ammo[AMMO_ROCKETS];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_HYPERBLASTER] || client->pers.inventory[IT_WEAPON_BFG] || client->pers.inventory[IT_WEAPON_IONRIPPER] || client->pers.inventory[IT_WEAPON_PLASMABEAM]) {
+			client->pers.inventory[IT_AMMO_CELLS] += 25;
+			if (client->pers.inventory[IT_AMMO_CELLS] > client->pers.max_ammo[AMMO_CELLS])
+				client->pers.inventory[IT_AMMO_CELLS] = client->pers.max_ammo[AMMO_CELLS];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_RAILGUN]) {
+			client->pers.inventory[IT_AMMO_SLUGS] += 6;
+			if (client->pers.inventory[IT_AMMO_SLUGS] > client->pers.max_ammo[AMMO_SLUGS])
+				client->pers.inventory[IT_AMMO_SLUGS] = client->pers.max_ammo[AMMO_SLUGS];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_PHALANX]) {
+			client->pers.inventory[IT_AMMO_MAGSLUG] += 9;
+			if (client->pers.inventory[IT_AMMO_MAGSLUG] > client->pers.max_ammo[AMMO_MAGSLUG])
+				client->pers.inventory[IT_AMMO_MAGSLUG] = client->pers.max_ammo[AMMO_MAGSLUG];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_ETF_RIFLE]) {
+			client->pers.inventory[IT_AMMO_FLECHETTES] += 25;
+			if (client->pers.inventory[IT_AMMO_FLECHETTES] > client->pers.max_ammo[AMMO_FLECHETTES])
+				client->pers.inventory[IT_AMMO_FLECHETTES] = client->pers.max_ammo[AMMO_FLECHETTES];
+		}
+
+		if (client->pers.inventory[IT_WEAPON_PROXLAUNCHER]) {
+			if (g_upgradeproxs->integer) {
+				client->pers.inventory[IT_AMMO_PROX] += 15;
+			}
+			else {
+				client->pers.inventory[IT_AMMO_PROX] += 6;
+			}
+			if (client->pers.inventory[IT_AMMO_PROX] > client->pers.max_ammo[AMMO_PROX]) {
+				client->pers.inventory[IT_AMMO_PROX] = client->pers.max_ammo[AMMO_PROX];
+			}
+		}
+
+		if (client->pers.inventory[IT_WEAPON_DISRUPTOR]) {
+			client->pers.inventory[IT_AMMO_ROUNDS] += 4;
+			if (client->pers.inventory[IT_AMMO_ROUNDS] > client->pers.max_ammo[AMMO_DISRUPTOR])
+				client->pers.inventory[IT_AMMO_ROUNDS] = client->pers.max_ammo[AMMO_DISRUPTOR];
+		}
+
+		client->pers.inventory[IT_AMMO_TESLA] += 2;
+		if (client->pers.inventory[IT_AMMO_TESLA] > client->pers.max_ammo[AMMO_TESLA])
+			client->pers.inventory[IT_AMMO_TESLA] = client->pers.max_ammo[AMMO_TESLA];
+
+		client->pers.inventory[IT_AMMO_TRAP] += 1;
+		if (client->pers.inventory[IT_AMMO_TRAP] > client->pers.max_ammo[AMMO_TRAP])
+			client->pers.inventory[IT_AMMO_TRAP] = client->pers.max_ammo[AMMO_TRAP];
+	}
+}
 
 /*
 ===============
