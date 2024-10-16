a108f2b811bf98978dcc48f00839e7bd7c8e70c8
Enemy-123 <kenny.q2@gmail.com>
2024-10-16 12:04:20 -0300

    restored armor/health/spheres/sentryguns if map has them, out of instagib

--- g_items.cpp
+++ g_items.cpp
@@ -1367,9 +1367,9 @@ void SpawnItem(edict_t* ent, gitem_t* item, const spawn_temp_t& st)
 		// [Kex] In instagib, spawn no pickups!
 		if (g_instagib->value)
 		{
-			if (item->pickup == Pickup_Armor || item->pickup == Pickup_PowerArmor ||
-				item->pickup == Pickup_Powerup || item->pickup == Pickup_Sphere || item->pickup == Pickup_sentrygun ||
-				(item->flags & IF_HEALTH) || (item->flags & IF_AMMO) || item->pickup == Pickup_Weapon || item->pickup == Pickup_Pack ||
+			if (/*item->pickup == Pickup_Armor ||*/ item->pickup == Pickup_PowerArmor ||
+				item->pickup == Pickup_Powerup /*|| item->pickup == Pickup_Sphere*/ || /*item->pickup == Pickup_sentrygun ||*/
+				/*(item->flags & IF_HEALTH) || */(item->flags & IF_AMMO) || item->pickup == Pickup_Weapon || item->pickup == Pickup_Pack ||
 				item->id == IT_ITEM_BANDOLIER || item->id == IT_ITEM_PACK ||
 				item->id == IT_AMMO_NUKE)
 			{
