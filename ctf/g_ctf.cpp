ca3ffe4ef9ea4fd0ffa0592715056afb22f80c10
Enemy-123 <kenny.q2@gmail.com>
2024-11-06 23:20:12 -0300

    0.086v

--- ctf/g_ctf.cpp
+++ ctf/g_ctf.cpp
@@ -2657,7 +2657,7 @@ static void SetGameName(pmenu_t* p)
 	if (ctf->integer)
 		Q_strlcpy(p->text, "$g_pc_3wctf", sizeof(p->text));
 	else
-		Q_strlcpy(p->text, "Horde MOD BETA v0.0085\n\n\n\n\n\n\n\n\nDiscord:\nEnemy0416", sizeof(p->text));
+		Q_strlcpy(p->text, "Horde MOD BETA v0.0086\n\n\n\n\n\n\n\n\nDiscord:\nEnemy0416", sizeof(p->text));
 }
 
 static void SetLevelName(pmenu_t* p)
