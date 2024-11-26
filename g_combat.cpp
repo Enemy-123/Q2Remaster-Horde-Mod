d72cee1c4d501fc1b3d403ad229439ac96f88cf3
Enemy-123 <kenny.q2@gmail.com>
2024-11-25 22:44:21 -0300

    fix react to damage resetting making monsters go weird

--- g_combat.cpp
+++ g_combat.cpp
@@ -872,11 +872,6 @@ void T_Damage(edict_t* targ, edict_t* inflictor, edict_t* attacker, const vec3_t
 	if (!targ->takedamage)
 		return;
 
-	// Actualizar react_to_damage_time para cualquier daño excepto agua
-	if (mod.id != MOD_WATER && (targ->svflags & SVF_MONSTER)) {
-		targ->monsterinfo.react_to_damage_time = level.time;
-	}
-
 	// Si es daño por agua, intentar teletransportar inmediatamente
 	if (mod.id == MOD_WATER && (targ->svflags & SVF_MONSTER) && !targ->monsterinfo.IS_BOSS) {
 		CheckAndTeleportStuckMonster(targ);
