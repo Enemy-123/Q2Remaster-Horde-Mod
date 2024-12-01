32090213bb495ffeb29105307d0847169970acca
Enemy-123 <kenny.q2@gmail.com>
2024-12-01 20:49:39 -0300

    tech dmg would be *2 easier now

--- ctf/g_ctf.cpp
+++ ctf/g_ctf.cpp
@@ -2599,7 +2599,7 @@ int CTFApplyStrength(edict_t* ent, int dmg) {
 	}
 
 	if (ent->client && dmg && ent->client->pers.inventory[IT_TECH_STRENGTH]) {
-		return ent->client->resp.spree <=15 ? dmg * 1.5f : dmg * 2.0f;
+		return (ent->client->resp.spree >= 10 || current_wave_level >= 15) ? dmg * 2.0f : dmg * 1.5f;
 	}
 
 	return dmg;
