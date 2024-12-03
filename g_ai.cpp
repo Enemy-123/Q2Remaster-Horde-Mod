3b519186723888914af379be63d40e688241137b
Enemy-123 <kenny.q2@gmail.com>
2024-12-03 02:15:31 -0300

    less strafe_chance, back to normal

--- g_ai.cpp
+++ g_ai.cpp
@@ -1436,9 +1436,9 @@ bool M_CheckAttack_Base(edict_t* self, float stand_ground_chance, float melee_ch
 			float strafe_chance;
 
 			if (!(strcmp(self->classname, "monster_daedalus")) || !(strcmp(self->classname, "monster_daedalus_bomber")))
-				strafe_chance = 0.92f;
+				strafe_chance = 0.8f;
 			else
-				strafe_chance = 0.75f;
+				strafe_chance = 0.6f;
 
 			// if enemy is tesla, never strafe
 			if ((self->enemy) && (self->enemy->classname) && (!strcmp(self->enemy->classname, "tesla_mine")))
