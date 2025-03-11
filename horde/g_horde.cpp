74c35a748194b42c7f204977fce5339e18456e10
Enemy-123 <kenny.q2@gmail.com>
2025-03-11 01:39:45 -0300

    a

--- horde/g_horde.cpp
+++ horde/g_horde.cpp
@@ -3962,12 +3962,11 @@ inline int8_t GetNumSpectPlayers() {
 
 // Implementación de DisplayWaveMessage
 static void DisplayWaveMessage(gtime_t duration = 5_sec) {
-	static const std::array<const char*, 5> messages = {
+	static const std::array<const char*, 4> messages = {
 		"Horde Menu available upon opening Inventory or using TURTLE on POWERUP WHEEL\n\nMAKE THEM PAY!\n",
 		"Welcome to Hell.\n\nUse FlipOff <Key> looking at walls to spawn lasers (cost: 25 cells)\n",
 		"New Tactics!\n\nTeslas can now be placed on walls and ceilings!\n\nUse them wisely!", 
-		"Improved Traps!\n\nTraps are reutilizable after 5secs of eating a strogg!\n\nExploding if strogg is bigger!, up to 60 seconds of life!", 
-		"Some Bonus were tweaked!\n\nBFG Pull will also slide at the same time, Ammo regen got improved,\n rest of bonuses with small changes"
+		"Improved Traps!\n\nTraps are reutilizable after 5secs of eating a strogg!\n\nExploding if strogg is bigger!, up to 60 seconds of life!"
 	};
 
 	// Usar distribución uniforme con mt_rand
