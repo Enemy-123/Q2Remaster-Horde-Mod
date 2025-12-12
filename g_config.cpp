fd063bda4ca753164d9aa16aded965d4769c529f
Enemy-123 <kenny.q2@gmail.com>
2025-12-12 02:24:40 -0300

    reducing error messages

--- g_config.cpp
+++ g_config.cpp
@@ -234,12 +234,21 @@ void Config_LoadMonsters(const char* basedir)
 		gi.Com_PrintFmt("Config: Loaded {} monster configurations from config/monsters.json\n", loaded_count);
 
 		// Verify all monster types have configs
+		// Skip non-spawnable entities: misc_* (easter eggs, decorative), boss3_stand, commander_body
 		int missing_count = 0;
 		for (uint8_t type_id = 0; type_id < static_cast<uint8_t>(horde::MonsterTypeID::MAX_TYPES); type_id++)
 		{
 			const char* classname = horde::MonsterTypeRegistry::GetClassname(static_cast<horde::MonsterTypeID>(type_id));
 			if (classname && classname[0])  // Valid monster type
 			{
+				// Skip non-spawnable entities that don't need config
+				if (strncmp(classname, "misc_", 5) == 0 ||       // Easter eggs, decorative entities
+				    strcmp(classname, "monster_boss3_stand") == 0 || // Decorative boss stand
+				    strcmp(classname, "monster_commander_body") == 0) // Decorative body
+				{
+					continue;
+				}
+
 				auto it = g_config.monsters.monsters.find(type_id);
 				if (it == g_config.monsters.monsters.end())
 				{
